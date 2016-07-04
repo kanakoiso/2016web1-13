f0ef9407d3682d06b1fbf7fdeb7c32f7789dbdb7
kanakoiso <g1527008@st.t-kougei.ac.jp>
2016-07-04 17:48:38 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,6 +6,7 @@ var separate_time = function(time){
   return [sec,min,hours,days];
 }
 
+var update = function(){
 var now = new Date();
 var target = new Date(2020,7,24,0,0,0,0);
 var diff = target.getTime() - now.getTime();
@@ -16,3 +17,11 @@ document.getElementById('countdown').textContent =
   counter[2] + '時間' +
   counter[1] + '分' +
   counter[0] + '秒' +
+refresh();//タイマーを起動
+}
+
+var refresh = functiom(){
+ setTimeout(update,1000);
+}
+
+update();
